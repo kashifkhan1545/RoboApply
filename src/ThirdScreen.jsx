72bb06d5bd8cf25d348a976d7179acd2810ccc96
Kashif Khan <kashifkhan26262@gmail.com>
2025-09-08 10:23:35 +0500

    changes

--- src/ThirdScreen.jsx
+++ src/ThirdScreen.jsx
@@ -1,5 +1,9 @@
 import { useNavigate } from "react-router-dom";
 import logo from "./assets/logo.png";
+import Active from "./assets/Active.png";
+import rd from "./assets/3rd.png";
+import rec from "./assets/rectangle.png"
+import over from "./assets/overlay.png"
 import "./ThirdScreen.css";
 
 function ThirdScreen() {
@@ -31,8 +35,18 @@ function ThirdScreen() {
       </div>
 
       {/* Right Panel */}
-      <div className="right-panel">
-        <div className="progress-container">
+      <div
+  className="right-panel"
+  style={{
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start", // push everything to top
+    alignItems: "center",     // align everything to left
+    gap: "5px",
+    paddingTop: "20px"            // optional spacing from top
+  }}
+>        <div className="progress-container">
           {/* First Progress Bar */}
           <div className="progress-item">
             <div className="progress-header">
@@ -63,6 +77,53 @@ function ThirdScreen() {
           <span className="white">search?</span>
           <span className="yellow">*</span>
         </h2>
+
+        {/* Note text */}
+        <p className="note-text">
+          <span className="yellow">Note:</span>{" "}
+          <span className="white">
+            Your selection will not affect your platform experience and is for informational purposes only.
+          </span>
+        </p>
+        {/* Three Boxes */}
+        <div className="box-container">
+          <div className="info-box">
+            <img src={Active} alt="box1" className="box-img" />
+            <h3 className="box-title">Active Job Seeker</h3>
+            <p className="box-detail">I am actively looking for new opportunities and want to find a role within the next six months.</p>
+          </div>
+
+          <div className="info-box">
+  <div className="box-img-container bigger-box">
+    <img src={rec} alt="box2" className="base-img" />
+    <img src={over} alt="overlay" className="overlay-img" />
+  </div>
+  <h3 className="box-title">Passive Job Seeker</h3>
+  <p className="box-detail">
+    I am passively browsing opportunities that can take me to the next level,
+    upskills me, or offers an interesting career transition.
+  </p>
+</div>
+
+
+
+          <div className="info-box">
+            <img src={rd} alt="box3" className="box-img" />
+            <h3 className="box-title">Not a Job Seeker</h3>
+            <p className="box-detail">I am currently not seeking new opportunities but would like to take advantage of career enhancing features such as resume score and Interview guide.</p>
+          </div>
+          
+        </div>
+        <div className="form-buttons" style={{alignSelf:'flex-end', marginRight:'12%'}}>
+              <button type="button" className="back-btn">Back</button>
+              <button 
+                type="button" 
+                className="next-btn"
+                onClick={() => navigate("/second")}
+              >
+                Next →
+              </button>
+            </div>
       </div>
     </div>
   );
